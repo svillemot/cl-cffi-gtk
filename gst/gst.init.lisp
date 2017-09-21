cb92e7e4362ab20510523d94e071cd01209c5b13
Olof-Joachim Frahm <olof@macrolet.net>
2017-09-21 21:53:47 +0200

    Fix library names, better initialisation.

--- gst/gst.init.lisp
+++ gst/gst.init.lisp
@@ -6,11 +6,13 @@
 (glib::at-init ()
   (eval-when (:compile-toplevel :load-toplevel :execute)
     (define-foreign-library gst
-      (t "libgstreamer-1.0.so.0"))
+      (t "libgstreamer-1.0.so"))
     (define-foreign-library gst-base
-      (t "libgstbase-1.0.so.0"))
+      (t "libgstbase-1.0.so"))
     (define-foreign-library gst-video
-      (t "libgstvideo-1.0.so.0")))
+      (t "libgstvideo-1.0.so"))
+    (define-foreign-library gst-video
+      (t "libgstvideo-1.0.so")))
   (unless (foreign-library-loaded-p 'gst)
     (use-foreign-library gst))
   (unless (foreign-library-loaded-p 'gst-base)
@@ -23,10 +25,20 @@
   (argv (:pointer (:pointer :string)))
   (err :pointer))
 
+;; TODO: this should possibly have an option to pass arguments that will
+;; influence GStreamer behaviour, but will require to defer the
+;; initialisation call to after all of that is known
 (defun %gst-init ()
-  (with-g-error (err)
-    (%gst-init-check (foreign-alloc :int :initial-element 0)
-                     (foreign-alloc :string :initial-contents '("/usr/bin/sbcl"))
-                     err)))
+  (let* ((binary #+ccl (car ccl:*command-line-argument-list*)
+                 #+sbcl (car sb-ext:*posix-argv*)
+                 ;; TODO: better fallback option, or error out
+                 #-(or ccl sbcl) "/usr/bin/sbcl")
+         (args `(,binary "--gst-disable-segtrap" "--gst-disable-registry-fork")))
+    (with-g-error (err)
+      (%gst-init-check
+       (foreign-alloc :int :initial-element 3)
+       (foreign-alloc :pointer :initial-element
+                      (foreign-alloc :string :initial-contents args))
+       err))))
 
 (glib::at-init () (%gst-init))
